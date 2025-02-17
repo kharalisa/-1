fd71a4230e91308888dd824c0a51e952d808c5a1
kharalisa <kharalisa1@gmail.com>
2025-02-17 19:34:38 +0400

    Add files via upload

---
+++ лаба №1.py
@@ -0,0 +1,41 @@
+slovar = {
+    '0': 'ноль',
+    '2': 'два',
+    '4': 'четыре',
+    '6': 'шесть'
+}
+
+def octal(c): 
+    return c in '01234567'
+
+def chet(c): 
+    return c in '1357'
+
+def replace_num(num): 
+    a = [] 
+    for d in num: 
+        if d in slovar:
+            a.append(slovar[d]) 
+        else:
+            a.append(d)
+    return ' '.join(a) 
+
+def read_file(file, size=1024): 
+    with open(file, 'r') as f: 
+        data = '' 
+        while True: 
+            block = f.read(size)
+            if not block: 
+                break
+            data += block 
+            while ' ' in data: 
+                pos = data.index(' ') 
+                leksema = data[:pos] 
+                data = data[pos+1:]
+                if len(leksema) <= 5 and all(octal(c) for c in leksema) and chet(leksema[0]):
+                    print(replace_num(leksema))
+
+read_file('numbers.txt')
+    
+    
+
